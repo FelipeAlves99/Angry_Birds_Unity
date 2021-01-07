62ea36a269efba9b606194799c721abd8c0b905f
FelipeAlves99 <falves99.dev@gmail.com>
2021-01-06 23:07:30 -0300

    Created first player movements

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    private Vector2 _startPosition;
+    private Rigidbody2D _rigidbody2D;
+    private SpriteRenderer _spriteRanderer;
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _spriteRanderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Start()
+    {
+        _startPosition = _rigidbody2D.position;
+        _rigidbody2D.isKinematic = true;
+    }
+
+    private void OnMouseDown()
+    {
+        _spriteRanderer.color = Color.red;
+    }
+
+    private void OnMouseUp()
+    {
+        var currentPosition = _rigidbody2D.position;
+        Vector2 direction = _startPosition - currentPosition;
+        direction.Normalize();
+
+        _rigidbody2D.isKinematic = false;
+        _rigidbody2D.AddForce(direction * 500);
+
+        _spriteRanderer.color = Color.white;
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(mousePosition.x, mousePosition.y, transform.position.z);
+    }
+
+    void Update()
+    {
+
+    }
+}
